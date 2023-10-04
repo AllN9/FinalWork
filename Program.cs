692fa3c88476a943e54071c8dc2c503587f5f9f0
Alexey < >
2023-10-05 00:05:12 +0300

    Добавлено решение задачи и исправлены неточности в описании

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//Используемые функции
+int Greeting()
+{
+    Console.Write("Введите размер массива: ");
+
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    return size;
+}
+string [] Filling(int size)
+{
+    Console.Write("Введите ваш массив: ");
+
+    string [] mas = new string [size];
+    for(int i = 0; i < size; i++)
+    {
+        mas[i] = Console.ReadLine();
+    }
+
+    return mas;
+}
+void Check(int size, string[] mas)
+{
+    Console.WriteLine("Введенный вами массив: ");
+
+    Console.Write("[\"" + mas[0]);
+    for(int i = 1; i < size; i++)
+    {
+        Console.Write("\", \"" + mas[i]);
+    }
+    Console.WriteLine("\"]");
+}
+void Result(int size, string[] mas)
+{
+    Console.WriteLine("Искомый массив: ");
+    
+    int length;
+    Console.Write("[");
+    for(int i = 0; i < size; i++)
+    {
+        length = mas[i].Length;
+        if(length <= 3)
+        {
+            Console.Write("\"" + mas[i] + "\"");
+        }
+    }
+    Console.WriteLine("]");    
+}
+//Реализация программы
+int size = Greeting();
+string [] mas = Filling(size);
+
+Check(size, mas);
+
+Result(size, mas);
